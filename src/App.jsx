2d7809a91630339a26ee2aacda6d1f5dca6395a8
ITP <RayhanUddinFarhad11@gmail.com>
2023-05-17 21:26:20 +0600

    Banner done

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Home from './components/pages/Home/Home'
+import Navbar from './components/shared/Navbar'
+import { Outlet } from 'react-router-dom'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div>
+
+      <Navbar></Navbar>
+      <div className='px-5'>
+
+      <Outlet></Outlet>
+      </div>
+
+     
+
+
+
+
+    </div>
+    </>
+  )
+}
+
+export default App
